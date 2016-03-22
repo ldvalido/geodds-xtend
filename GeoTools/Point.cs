70897d8d266243684333e83b9f291d995c8a196c
ldvalido <ldvalido@users.noreply.github.com>
2016-03-22 16:39:35 -0300

    Added files via upload
    Add Files for Main Project

--- GeoTools/Point.cs
+++ GeoTools/Point.cs
@@ -1,4 +1,4 @@
-namespace GeoTools
+namespace GeoddsXtendgeoddsXtend
 {
     public class Point
     {
--- GeoTools/PointExtension.cs
+++ GeoTools/PointExtension.cs
@@ -1,18 +1,25 @@
-namespace GeoTools
+using System;
+using System.Device.Location;
+
+namespace GeoddsXtendgeoddsXtend
 {
     public static class GeoTools
     {
         public static double Distance(this Point point, Point anotherPoint)
         {
             if (point != null && anotherPoint != null){
-                return GisHelper.DistanceTo(
-                    point.Latitude,
-                    point.Longitude,
-                    anotherPoint.Latitude,
-                    anotherPoint.Longitude
-                    );
+
+                var geoSource = new GeoCoordinate(point.Latitude, point.Longitude);
+
+                var geoTarget = new GeoCoordinate(anotherPoint.Latitude, anotherPoint.Longitude);
+
+                var distanceInMeters = geoSource.GetDistanceTo(geoTarget);
+
+                var returnValue = distanceInMeters/1000;
+
+                return returnValue;
             }else{
-                throw new NullArgumentException("Some is point.");
+                throw new ArgumentNullException("Some point is null");
             }
         } 
     }
