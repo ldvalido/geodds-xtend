266fa3aaffd0fd63c3eae34672c6522c5ec09ebe
lvalido <lvalido@mailexternos.com.ar>
2016-03-23 10:05:46 -0300

    [GeoTools] Add Tests for polygon

---
+++ Geodds-xtend.Test/PolygonTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace GeoddsXtend.Test
+{
+    public class PolygonTest
+    {
+
+        [Fact]
+        public void TestBasic()
+        {
+            var vertexes = new List<Point>
+            {
+                new Point(10, 10),
+                new Point(10, 20),
+                new Point(20, 10),
+                new Point(20, 20)
+            };
+            var polygon = new Polygon(vertexes.ToArray());
+
+            var belongsTo = polygon.IsInside(new Point(15, 15));
+            Assert.True(belongsTo);
+
+            var dontBelognsTo = polygon.IsInside(new Point(5, 5));
+            Assert.False(dontBelognsTo);
+        }
+    }
+}
